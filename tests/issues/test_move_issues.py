99dcfaa76865a8a30165a6939a5bd02280bd9c35
Thomas Loubrieu <loubrieu@jpl.nasa.gov>
2022-03-03 11:22:38 -0800

    resolve conflicts

--- tests/issues/test_move_issues.py
+++ tests/issues/test_move_issues.py
@@ -6,15 +6,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_move_issue(self):
         gh_connection = get_gh_connection()
-<<<<<<< HEAD
-<<<<<<< HEAD
         move_issues('NASA-PDS/registry-api', 'NASA-PDS/registry', gh_connection, label='model', dry_run=True)
-=======
-        move_issues('NASA-PDS/pds-api-javalib', 'NASA-PDS/registry-api', gh_connection, label='model', dry_run=False)
->>>>>>> add script to move issue from one repository to another
-=======
-        move_issues('NASA-PDS/registry-api', 'NASA-PDS/registry', gh_connection, label='model', dry_run=True)
->>>>>>> updates after peer review
 
 
 
